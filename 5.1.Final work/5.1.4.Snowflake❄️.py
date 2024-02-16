d890efca9caedd9adf2a6a0f9d986a88a52f4d31
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-16 17:43:57 -0500

    Update and rename 5.1.4.Snowflake.py to 5.1.4.Snowflake❄️.py

--- 5.1.Final work/5.1.4.Snowflake❄️.py
+++ 5.1.Final work/5.1.4.Snowflake❄️.py
@@ -1,10 +1,10 @@
-# Snowflake
+# Snowflake ❄️
 # An odd natural number n is given as input to the program.
 # Write a program that creates a matrix of size n×n filling it with symbols. 
 # Then fill in the middle row and column of the matrix, the main and secondary diagonals of 
 # the matrix with * symbols. Display the resulting matrix on the screen, separating elements with spaces.
 
-# Снежинка
+# Снежинка ❄️
 # На вход программе подается нечетное натуральное число n. Напишите программу, которая создает матрицу размером n×n заполнив её символами. 
 # Затем заполните символами * среднюю строку и столбец матрицы, главную и побочную диагональ матрицы. Выведите полученную матрицу на экран, разделяя элементы пробелами.
 
