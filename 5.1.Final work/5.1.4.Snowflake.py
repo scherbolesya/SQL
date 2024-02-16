216f7ebc02e1a8179e11669b7cdaa7be17d9a002
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-16 17:42:34 -0500

    Create 5.1.4.Snowflake.py

---
+++ 5.1.Final work/5.1.4.Snowflake.py
@@ -0,0 +1,54 @@
+# Snowflake
+# An odd natural number n is given as input to the program.
+# Write a program that creates a matrix of size n×n filling it with symbols. 
+# Then fill in the middle row and column of the matrix, the main and secondary diagonals of 
+# the matrix with * symbols. Display the resulting matrix on the screen, separating elements with spaces.
+
+# Снежинка
+# На вход программе подается нечетное натуральное число n. Напишите программу, которая создает матрицу размером n×n заполнив её символами. 
+# Затем заполните символами * среднюю строку и столбец матрицы, главную и побочную диагональ матрицы. Выведите полученную матрицу на экран, разделяя элементы пробелами.
+
+n = int(input())
+matrix = [['.']*n for _ in range(n)] 
+for i in range(n): 
+    matrix[i][i] = '*' #главная диагональ
+    matrix[i][~i] = '*' #побочная диагональ
+    matrix[i][n//2] = '*'
+    matrix[n//2][i] = '*'
+for row in matrix:
+    print(*row)
+
+Sample Input 1:
+5
+Sample Output 1:
+* . * . *
+. * * * .
+* * * * *
+. * * * .
+* . * . *
+
+Sample Input 2:
+7
+Sample Output 2:
+* . . * . . *
+. * . * . * .
+. . * * * . .
+* * * * * * *
+. . * * * . .
+. * . * . * .
+* . . * . . *
+
+Sample Input 3:
+11
+Sample Output 3:
+* . . . . * . . . . *
+. * . . . * . . . * .
+. . * . . * . . * . .
+. . . * . * . * . . .
+. . . . * * * . . . .
+* * * * * * * * * * *
+. . . . * * * . . . .
+. . . * . * . * . . .
+. . * . . * . . * . .
+. * . . . * . . . * .
+* . . . . * . . . . *
