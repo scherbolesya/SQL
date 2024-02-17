5c39f1b003834fcd47aa91f42af232b600d28eb6
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 16:56:06 -0500

    Create 7.1.16.py

---
+++ 7.Final work/7.1.16.py
@@ -0,0 +1,6 @@
+# Что будет храниться в переменной b после выполнения следующего кода?
+# What will be stored in variable c after the following code is executed?
+
+a, *b, c = 'No bees', 'no honey'
+
+# []
