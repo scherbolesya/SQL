5b554b158ebd7597908818ef4b1ef5043a715254
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 18:23:10 -0500

    Create 8.2.11.py

---
+++ 8.Sets/8.2.Set operations, Euler-Venn diagrams/8.2.11.py
@@ -0,0 +1,11 @@
+# В классе учатся 30 учеников. Среди них 17 отличников по математике, 10 отличников по физике и 13 — по информатике. 
+# Трое — отличники по всем предметам, пятеро — по математике и физике, четверо — по физике и информатике, 
+# а 6 человек — по математике и информатике. Сколько учеников не являются отличниками ни по одному из этих предметов?
+
+# There are 30 students in the class. Among them are 17 excellent students in mathematics, 
+# 10 excellent students in physics and 13 excellent students in computer science. 
+# Three are excellent students in all subjects, five are in mathematics and physics, 
+# four are in physics and computer science, and 6 people are in mathematics and computer science. 
+# How many students are not excellent in any of these subjects?
+
+# 2
