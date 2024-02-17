6efc70f05a2b2b6f3f6bb2636590aa436384101f
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 16:47:20 -0500

    Create 7.1.8.py

---
+++ 7.Final work/7.1.8.py
@@ -0,0 +1,5 @@
+# кортеж – это упорядоченный набор элементов
+# a tuple is an ordered collection of elements
+
+# кортеж нельзя изменять после создания
+# a tuple cannot be modified after it is created
