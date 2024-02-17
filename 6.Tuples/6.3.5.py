791ae84bdd7d35dc6a3c2f7004f32b559e8a8e07
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-16 22:37:31 -0500

    Create 6.3.5.py

---
+++ 6.Tuples/6.3.5.py
@@ -0,0 +1,5 @@
+# Дополните приведенный код так, чтобы он преобразовал строку в кортеж символов строки и вывел результат.
+# Complete the given code so that it converts the string to a tuple of string characters and prints the result.
+
+data = 'Python для продвинутых!'
+print(tuple(data))
