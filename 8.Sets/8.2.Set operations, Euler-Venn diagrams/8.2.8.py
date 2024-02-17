d7ba3eaa54c8baae490723621a198607ff3c5adf
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 18:17:05 -0500

    Create 8.2.8.py

---
+++ 8.Sets/8.2.Set operations, Euler-Venn diagrams/8.2.8.py
@@ -0,0 +1,4 @@
+# Множества A и B содержат 5 и 6 элементов соответственно, а множество A∩B – 2 элемента. Сколько элементов в множестве A∪B?
+# The sets A and B contain 5 and 6 elements, respectively, and the set A∩B contains 2 elements. How many elements are there in the set A∪B?
+
+# 9
