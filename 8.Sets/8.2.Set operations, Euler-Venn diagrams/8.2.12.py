e230b9007c62b6ec0fdc2171631b2ada60902bd8
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 18:24:47 -0500

    Create 8.2.12.py

---
+++ 8.Sets/8.2.Set operations, Euler-Venn diagrams/8.2.12.py
@@ -0,0 +1,11 @@
+# В классе 36 учеников, из которых двое не знают иностранных языков. 
+# Английским языком владеют 25 учеников, немецким — 11, французским — 17 человек. 
+# Известно, что и английским, и немецким языком владеют 6 учеников, и английским, и французским — 10 учеников, 
+# и немецким, и французским — 4 ученика. Сколько учеников владеют всеми тремя языками?
+
+# There are 36 students in the class, two of whom do not know foreign languages. 
+# 25 students speak English, 11 speak German, and 17 speak French. 
+# It is known that 6 students speak both English and German, 10 students speak both English and French, 
+# and 4 students speak German and French. How many students speak all three languages?
+
+# 1
