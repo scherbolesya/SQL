c6e65d8e9842b890244c851d45270ff623611d8e
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-16 22:56:10 -0500

    Create 7.1.3.py

---
+++ 7.Final work/7.1.3.py
@@ -0,0 +1,8 @@
+# Что будет выведено на экран в результате работы следующей программы?
+
+# What will be displayed on the screen as a result of the following program?
+
+tpl = (100,)
+print(tpl * 2)
+
+# (100, 100)
