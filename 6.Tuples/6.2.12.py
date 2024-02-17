9f5ff107843d8061fc8c8ae2e66c2a8c4b1a2345
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-16 22:27:11 -0500

    Create 6.2.12.py

---
+++ 6.Tuples/6.2.12.py
@@ -0,0 +1,6 @@
+# Дополните приведенный код так, чтобы переменная number, содержала количество вхождений «Spain» в кортеж countries.
+# Complete the code below so that the number variable contains the number of occurrences of “Spain” in the countries tuple.
+
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Spain', 'Cameroon')
+number = countries.count('Spain')
+print(number)
