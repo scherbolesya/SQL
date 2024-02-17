db64d8b99aabf87d1df0ad97592957a64d9dd6c6
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 16:53:43 -0500

    Create 7.1.14.py

---
+++ 7.Final work/7.1.14.py
@@ -0,0 +1,6 @@
+# Что будет храниться в переменной b после выполнения следующего кода?
+# What will be stored in variable b after the following code is executed?
+
+a, b, c = 'Beegeek loves Stepik'.split()
+
+# 'loves'
