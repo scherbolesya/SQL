62b49365af1af60c13b2e1b73aef68dc18bf102a
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-16 22:22:02 -0500

    Create 6.2.6.py

---
+++ 6.Tuples/6.2.6.py
@@ -0,0 +1,8 @@
+# Complete the following code using slices so that it displays the elements of the countries tuple other than the first two.
+# Note. The output should be the string ('Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon').
+
+# Дополните приведенный код, используя срезы, так чтобы он вывел элементы кортежа countries кроме первых двух.
+# Примечание. Результатом вывода должна быть строка ('Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon').
+
+countries = ('Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
+print(countries[2:])
