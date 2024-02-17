ab75ec410efc049b7564470faaeb3c8a561a878a
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 16:50:25 -0500

    Create 7.1.11.py

---
+++ 7.Final work/7.1.11.py
@@ -0,0 +1,7 @@
+# Что будет выведено на экран в результате работы следующей программы?
+# What will be displayed on the screen as a result of the following program?
+
+tpl = ('Green',)
+print(type(tpl))
+
+# <class 'tuple'>
