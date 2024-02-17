4dbb8eb9d93b83cbf3a4bb4c7c57f3363c2d79dd
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 18:18:36 -0500

    Create 8.2.9.py

---
+++ 8.Sets/8.2.Set operations, Euler-Venn diagrams/8.2.9.py
@@ -0,0 +1,7 @@
+# Каждый ученик в классе изучает английский или французский язык. Английский язык изучает 25 человек, французский – 27, 
+# а оба языка – 18. Сколько учащихся в классе?
+
+# Every student in the class learns English or French. There are 25 students studying English, 
+# 27 studying French, and 18 studying both languages. How many students are in the class?
+
+# 34
