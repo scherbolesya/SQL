19b4d39c6155a21f5cc211fc013fecf06f9185e2
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 16:48:25 -0500

    Create 7.1.9.py

---
+++ 7.Final work/7.1.9.py
@@ -0,0 +1,8 @@
+# Что будет выведено на экран в результате работы следующей программы?
+# What will be displayed on the screen as a result of the following program?
+
+tpl = (100, 200, 300, 400, 500)
+tpl[1] = 700
+print(tpl)
+
+# ошибка во время выполнения кода
