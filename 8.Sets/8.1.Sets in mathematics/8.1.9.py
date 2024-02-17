a865c720cca50b825744e1c1d77c9f3695758e6c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 17:13:01 -0500

    Create 8.1.9.py

---
+++ 8.Sets/8.1.Sets in mathematics/8.1.9.py
@@ -0,0 +1,5 @@
+# Понятие об этих числах возникло в связи с потребностью счёта предметов ещё в доисторические времена. Как называются эти числа?
+# The concept of these numbers arose in connection with the need to count objects back in prehistoric times. What are these numbers called?
+
+# натуральные
+# natural
