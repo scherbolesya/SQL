36214ebde05cad2fc9c5c8bf698ec594d8cd324c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-16 22:26:24 -0500

    Create 6.2.11.py

---
+++ 6.Tuples/6.2.11.py
@@ -0,0 +1,6 @@
+# Дополните приведенный код так, чтобы переменная index содержала индекс элемента «Slovenia» в кортеже countries.
+# Complete the code below so that the index variable contains the index of the "Slovenia" element in the countries tuple.
+
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+index = countries.index('Slovenia')
+print(index)
