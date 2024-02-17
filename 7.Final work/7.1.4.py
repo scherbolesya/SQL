06363dbab8033801933e8d34258cc2d6b463a07d
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-16 22:57:21 -0500

    Create 7.1.4.py

---
+++ 7.Final work/7.1.4.py
@@ -0,0 +1,8 @@
+# Что будет выведено на экран в результате работы следующей программы?
+
+# What will be displayed on the screen as a result of the following program?
+
+tpl = (10, 20, 30, 40, 50, 60, 70, 80)
+print(tpl[2:5], tpl[:4], tpl[3:])
+
+# (30, 40, 50) (10, 20, 30, 40) (40, 50, 60, 70, 80)
