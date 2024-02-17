61be784a3ddeff895ae0088a46ef2ef8a96fceb4
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 16:54:50 -0500

    Create 7.1.15.py

---
+++ 7.Final work/7.1.15.py
@@ -0,0 +1,6 @@
+# Что будет храниться в переменной c после выполнения следующего кода?
+# What will be stored in variable c after the following code is executed?
+
+a, b, *c = range(7)
+
+# [2, 3, 4, 5, 6]
