7945ad587ddc922812464280d2c322d4108f12aa
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 16:51:29 -0500

    Create 7.1.12.py

---
+++ 7.Final work/7.1.12.py
@@ -0,0 +1,8 @@
+# Что будет выведено на экран в результате работы следующей программы?
+# What will be displayed on the screen as a result of the following program?
+
+tpl = (100, 200, 300, 400, 500)
+tpl.pop(2)
+print(tpl)c
+
+# ошибка во время выполнения кода
