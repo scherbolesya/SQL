2eef7d061cad877a68f46a1334af83d8f0409f7c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 16:46:05 -0500

    Create 7.1.7.py

---
+++ 7.Final work/7.1.7.py
@@ -0,0 +1,8 @@
+# Что будет выведено на экран в результате работы следующей программы?
+# What will be displayed on the screen as a result of the following program?
+
+tpl = (1777, 'a')
+print(max(tpl))
+
+# ошибка во время выполнения кода
+# error during code execution
