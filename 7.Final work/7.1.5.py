0c92e1a58dd978e8b6bb2426fbd671d987213a95
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 16:43:14 -0500

    Create 7.1.5.py

---
+++ 7.Final work/7.1.5.py
@@ -0,0 +1,9 @@
+# What will be displayed on the screen as a result of the following program?
+# Что будет выведено на экран в результате работы следующей программы? 
+
+tpl = (100, 200, 300, 400, 500)
+print(tpl[-2])
+print(tpl[-4:-1])
+
+# 400
+# (200, 300, 400)
