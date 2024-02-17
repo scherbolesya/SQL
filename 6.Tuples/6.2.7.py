518b5de4e75aec2b0c837ebd05a729f3637590c4
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-16 22:22:51 -0500

    Create 6.2.7.py

---
+++ 6.Tuples/6.2.7.py
@@ -0,0 +1,5 @@
+# Дополните приведенный код, используя срезы, чтобы он вывел все элементы кортежа countries, кроме последних трех.
+# Complete the following code using slices so that it prints all but the last three elements of the countries tuple.
+
+countries = ('Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
+print(countries[:-3])
