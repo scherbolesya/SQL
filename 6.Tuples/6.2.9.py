1eb01c6cb408821395d4c9664586e605e9ff5e99
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-16 22:24:35 -0500

    Create 6.2.9.py

---
+++ 6.Tuples/6.2.9.py
@@ -0,0 +1,6 @@
+# Дополните приведенный код так, чтобы переменная number содержала количество элементов кортежа countries.
+# Complete the code below so that the number variable contains the number of elements of the countries tuple.
+
+countries = ('Romania', 'Poland', 'Estonia', 'Bulgaria', 'Slovakia', 'Slovenia', 'Hungary')
+number = len(countries)
+print(number)
