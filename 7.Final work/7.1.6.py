f72deb7382aab1cfbc1ea9b11d4f183604da0b05
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 16:44:25 -0500

    Create 7.1.6.py

---
+++ 7.Final work/7.1.6.py
@@ -0,0 +1,6 @@
+# Выберите правильный способ получения значения 20 из кортежа:
+# Choose the correct way to get the value 20 from a tuple:
+
+tpl = ('Orange', [10, 20, 30], (5, 15, 25))
+
+# tpl[1][1]
