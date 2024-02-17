bfb9b59547abffe79fafa4337a3ede56af3a49b3
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 18:20:04 -0500

    Create 8.2.10.py

---
+++ 8.Sets/8.2.Set operations, Euler-Venn diagrams/8.2.10.py
@@ -0,0 +1,8 @@
+# В одном из классов онлайн-школы BEEGEEK учится 67 человек. Из них 47 умеют решать задачи с параметрами, 
+# 35 –экономические задачи, а 23 – и те и другие. Сколько человек в классе не умеют решать ни экономические задачи, ни задачи с параметрами?
+
+# There are 67 students in one of the BEEGEEK online school classes. Of these, 
+# 47 are able to solve problems with parameters, 35 are able to solve economic problems, 
+# and 23 are able to solve both. How many people in the class cannot solve either economic problems or problems with parameters?
+
+# 8
