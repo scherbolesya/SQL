0658446703a4292c9d9f148847cd68dc72ac8616
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 17:10:01 -0500

    Create 10.2.4.py

---
+++ 10.Dictionaries/10.2.Basics of working with dictionaries/10.2.4.py
@@ -0,0 +1,8 @@
+# Что выведет следующий программный код?
+# What will the following program code output?
+
+dict1 = {'key1':1, 'key2':2}
+dict2 = {'key2':2, 'key1':1}
+print(dict1 == dict2)
+
+# True
