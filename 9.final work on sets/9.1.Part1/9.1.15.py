3800129189c2a8ac9764688a476e9a50c455a86b
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:43:40 -0500

    Create 9.1.15.py

---
+++ 9.final work on sets/9.1.Part1/9.1.15.py
@@ -0,0 +1,6 @@
+# Наполните множество set1 содержимым так, чтобы программа вывела {'p'}.
+# Fill set1 with contents so that the program prints {'p'}.
+
+set1 = {'a', 't', 'f', 'p'}
+set2 = {'a', 't', 'f'}
+print(set1 - set2)
