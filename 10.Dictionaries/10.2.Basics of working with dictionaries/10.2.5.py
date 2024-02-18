227d8c0ef124131775b5ede36b918ed10ff5d013
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 17:11:07 -0500

    Create 10.2.5.py

---
+++ 10.Dictionaries/10.2.Basics of working with dictionaries/10.2.5.py
@@ -0,0 +1,7 @@
+# Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+
+my_dict = {'foo': 100, 'bar': 200, 'baz': 300}
+print(my_dict['bar':'baz'])
+
+# ошибка во время выполнения
