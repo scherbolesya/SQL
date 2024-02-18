3d49d3406616280f30fd91343a7a8cb5774365f6
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:42:48 -0500

    Create 9.1.14.py

---
+++ 9.final work on sets/9.1.Part1/9.1.14.py
@@ -0,0 +1,8 @@
+# Что может быть выведено в результате выполнения следующего программного кода? 
+# What might be output as a result of executing the following code?
+
+myset = {'Yellow', 'Orange', 'Black'}
+myset.discard('Blue')
+print(myset)
+
+# {'Yellow', 'Orange', 'Black'}
