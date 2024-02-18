d0d8d0381f185676dcb78f37a20e0b0357552613
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:33:55 -0500

    Create 9.1.7.py

---
+++ 9.final work on sets/9.1.Part1/9.1.7.py
@@ -0,0 +1,5 @@
+# Верно ли утверждение: метод isdisjoint() возвращает значение True, если множества не имеют общих элементов и значение False если множества имеют общие элементы?
+# Is it true that the isdisjoint() method returns True if the sets do not have common elements and False if the sets have common elements?
+
+# да
+# yes
