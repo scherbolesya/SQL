836644ca07f08bbe644b23750cb85d293f5e06c7
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 21:28:21 -0500

    Create 8.5.2.py

---
+++ 8.Sets/8.5.Set methods/8.5.2.py
@@ -0,0 +1,4 @@
+# How can I add the value of a new item element to an already existing set myset?
+# Как можно добавить значение нового элемента item в уже существующее множество myset?
+
+myset.add(item)
