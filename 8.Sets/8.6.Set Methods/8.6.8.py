40acda17fd44f487538add42e4d6f11ad148bd01
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 22:05:32 -0500

    Create 8.6.8.py

---
+++ 8.Sets/8.6.Set Methods/8.6.8.py
@@ -0,0 +1,10 @@
+# Какие элементы будут храниться в множестве set3 в результате выполнения следующего программного кода? 
+# What elements will be stored in set3 when the following code is executed?
+
+set1 = set(['a', 'б', 'в', 'д'])
+set2 = set(['б', 'в', 'г'])
+set3 = set1.symmetric_difference(set2)
+
+# г
+# а
+# д
