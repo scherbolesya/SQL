49089b844e7124159506b9a6163197cda9a47c86
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 22:26:21 -0500

    Create 8.7.6.py

---
+++ 8.Sets/8.7.Set Methods/8.7.6.py
@@ -0,0 +1,8 @@
+# Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+
+set1 = {'q', 'w', 'e', 'r', 't', 'y'}
+list1 = ['y', 'w', 'r']
+print(set1 >= list1)
+
+# ошибка во время выполнения
