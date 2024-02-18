a8d7ad1e12329ac81472dc41b18a7f2842b3c850
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:40:58 -0500

    Create 9.1.12.py

---
+++ 9.final work on sets/9.1.Part1/9.1.12.py
@@ -0,0 +1,7 @@
+# Что будет выведено в результате выполнения следующего программного кода? 
+# What will be the output of the following code?
+
+myset = {'Yellow', 'Orange', 'Black'}
+print(myset[1])
+
+# ошибка во время выполнения
