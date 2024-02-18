37a084642f17d67fc70c8e784ce16ec3fe7fad21
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:21:56 -0500

    Create 8.8.11.py

---
+++ 8.Sets/8.8.Set generators and frozenset/8.8.11.py
@@ -0,0 +1,5 @@
+# Можно ли сравнивать обычное множество (тип set) и замороженное множество (тип frozenset)?
+# Is it possible to compare a regular set (set type) and a frozen set (frozenset type)?
+
+# да
+# yes
