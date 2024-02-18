efb22833ea5555f2fb90a04320520052aa9d6924
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:26:40 -0500

    Create 8.8.15.py

---
+++ 8.Sets/8.8.Set generators and frozenset/8.8.15.py
@@ -0,0 +1,9 @@
+# Какие элементы будут храниться в замороженном множестве set3 в результате выполнения следующего программного кода? 
+# What elements will be stored in the frozen set3 as a result of executing the following code?
+
+set1 = frozenset('beegeek')
+set2 = frozenset('stepik')
+set3 = set1 & set2
+
+k
+e
