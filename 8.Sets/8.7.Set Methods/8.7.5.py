352d212f7f0ef2eb22f8c4fd35e7065d6ff9f867
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 22:25:25 -0500

    Create 8.7.5.py

---
+++ 8.Sets/8.7.Set Methods/8.7.5.py
@@ -0,0 +1,8 @@
+# Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+
+set1 = {1, 2, 3, 4, 5, 6, 7, 8}
+list1 = [1, 2, 3, 4, 5]
+print(set1.issuperset(list1))
+
+# True
