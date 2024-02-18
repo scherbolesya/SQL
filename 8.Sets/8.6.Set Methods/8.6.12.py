778b00f8afed72619d53bdcd96eeb52982df0fa2
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 22:13:26 -0500

    Create 8.6.12.py

---
+++ 8.Sets/8.6.Set Methods/8.6.12.py
@@ -0,0 +1,9 @@
+# Какие элементы будут храниться в множестве set3 в результате выполнения следующего программного кода? 
+# What elements will be stored in set3 when the following code is executed?
+
+set1 = {'a', 'b', 'c', 'd', 'h'}
+set2 = {'b', 'd', 'f', 'h'}
+set3 = set1 - set2 & set1
+
+# a
+# c
