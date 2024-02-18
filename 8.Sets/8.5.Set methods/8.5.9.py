8d43855d14f1af8af436d464da56012f746e707c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 21:37:13 -0500

    Create 8.5.9.py

---
+++ 8.Sets/8.5.Set methods/8.5.9.py
@@ -0,0 +1,8 @@
+# Что будет выведено в результате выполнения следующего программного кода?
+# What will be the output of the following code?
+
+myset = set()
+item = myset.pop()
+print(item)
+
+# ошибка во время выполнения
