3dcb9f9b812269a4efd531d7ae4fdf19e1c4dd2c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:32:25 -0500

    Create 9.1.5.py

---
+++ 9.final work on sets/9.1.Part1/9.1.5.py
@@ -0,0 +1,5 @@
+# Верно ли утверждение: метод union() возвращает новое множество со всеми элементами из обоих множеств, удаляя дубликаты?
+# Is it true that the union() method returns a new set with all the elements from both sets, removing duplicates?
+
+# да
+# yes
