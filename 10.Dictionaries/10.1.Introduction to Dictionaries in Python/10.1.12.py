0f558bbb36e8cfe0be895c029747fb369629112e
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 16:42:44 -0500

    Create 10.1.12.py

---
+++ 10.Dictionaries/10.1.Introduction to Dictionaries in Python/10.1.12.py
@@ -0,0 +1,10 @@
+# Какие значения связаны с ключами в соответствующих словарях?
+# What values are associated with the keys in the corresponding dictionaries?
+
+int_dict = {2: 'two', 5: 'five', 7: 'seven', 9: 'nine'}
+str_dict = {'three': 3, 'seven': 7, 'nine': 9}
+
+int_dict[2] --- two
+int_dict[7] --- seven
+str_dict['seven'] --- 7
+str_dict['nine'] --- 9
