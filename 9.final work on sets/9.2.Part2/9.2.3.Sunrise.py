7cf381158066821be3362ebaeb84ee9763cb4afd
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 16:12:56 -0500

    Update 9.2.3.Sunrise.py

--- 9.final work on sets/9.2.Part2/9.2.3.Sunrise.py
+++ 9.final work on sets/9.2.Part2/9.2.3.Sunrise.py
@@ -3,10 +3,6 @@
 # Every minute it transmits to the observatory via a communication channel a positive integer - the amount of solar radiation energy. 
 # To properly analyze the results, there is no need to keep duplicate data, so they need to be removed. 
 # Write a program that prints the maximum number of satellite readings that, if removed, will correctly analyze the result.
-# Input format
-# The input to the program is one line containing numbers - readings from the Voskhod satellite. Numbers are separated by spaces and do not contain leading zeros.
-# Output format
-# The program should display the maximum number of readings, after removing which the results will be analyzed correctly.
 # Note. Let's consider test 1: we have data 10 20 30 10 40 10. We see that the number 10 is contained here 3 times - which means that duplicate numbers 10 must be removed. We have 2 of these (we do not take into account the first number 10). The other numbers are not repeated, so the answer is 2.
 
 # Восход
@@ -14,10 +10,6 @@
 # Каждую минуту он передаёт в обсерваторию по каналу связи положительное целое число — количество энергии солнечного излучения. 
 # Для правильного анализа результатов нет необходимости держать повторяющиеся данные, поэтому их нужно удалить. 
 # Напишите программу, которая выводит максимальное количество показаний спутника, при удалении которых результат будет правильно проанализирован.
-# Формат входных данных
-# На вход программе подаётся одна строка, содержащая числа – показания спутника «Восход». Числа указываются через пробел и не содержат ведущих нулей.
-# Формат выходных данных
-# Программа должна вывести максимальное количество показаний, после удаления которых анализ результатов будет произведен верно.
 # Примечание. Рассмотрим 1 тест: у нас подаются данные 10 20 30 10 40 10. Мы видим, что число 10 содержится тут 3 раза – значит, повторяющиеся числа 10 надо удалить. 
 # Таких у нас 2 (первое число 10 мы не учитываем). Другие числа не повторяются, поэтому ответ будет 2.
 
