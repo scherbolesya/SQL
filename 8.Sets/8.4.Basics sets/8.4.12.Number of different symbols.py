734e3abcc8fd53391682672b7be6e45692ca3079
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 21:14:56 -0500

    Create 8.4.12.Number of different symbols.py

---
+++ 8.Sets/8.4.Basics sets/8.4.12.Number of different symbols.py
@@ -0,0 +1,16 @@
+# Number of different symbols
+# A line of text is given as input to the program. Write a program that determines the number of distinct characters in a string.
+
+# Количество различных символов
+# На вход программе подается строка текста. Напишите программу, которая определяет количество различных символов в строке.
+
+print(len(set(input())))
+
+# Sample Input 1:
+# 12345678910
+# Sample Output 1:
+# 10
+# Sample Input 2:
+# ab bc
+# Sample Output 2:
+# 4
