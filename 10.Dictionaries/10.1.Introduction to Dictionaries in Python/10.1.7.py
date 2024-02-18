14a9bf4b014d3a1f7710dec375bd069dca82ab2a
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 16:37:09 -0500

    Create 10.1.7.py

---
+++ 10.Dictionaries/10.1.Introduction to Dictionaries in Python/10.1.7.py
@@ -0,0 +1,7 @@
+# Что увидим в результате выполнения приведенного ниже фрагмента кода?
+# What will we see as a result of executing the code fragment below?
+
+stuff = {1: 'ааа', 2: 'ббб', 3: 'ввв'}
+print(stuff[3])
+
+# ввв
