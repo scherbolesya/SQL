0b6cb2ccf174fd9907f7d0a7faae90c4c0909142
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 16:33:52 -0500

    Create 10.1.4.py

---
+++ 10.Dictionaries/10.1.Introduction to Dictionaries in Python/10.1.4.py
@@ -0,0 +1,5 @@
+# Предположим, что пара значений 'beegeek':2018 является элементом некоторого словаря. Что служит ключом? И что - значением?
+# Suppose the value pair 'beegeek':2018 is an element of some dictionary. What is the key? And what is the meaning?
+
+# 2018 – значение
+# 'beegeek' – ключ
