7be50713ec90178e04a2e2e58f14f6d41fe6c1e5
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 22:06:46 -0500

    Create 8.6.9.py

---
+++ 8.Sets/8.6.Set Methods/8.6.9.py
@@ -0,0 +1,12 @@
+# Какие элементы будут храниться в множестве myset в результате выполнения следующего программного кода? 
+# What elements will be stored in myset when the following code is executed?
+
+myset = set([10, 9, 8, 3])
+myset.update([1, 2, 3])
+
+# 9
+# 8
+# 10
+# 2
+# 3
+# 1
