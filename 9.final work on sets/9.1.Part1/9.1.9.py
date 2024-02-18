ae6aa0fdf9a111ceaf2ee51d2144a548a80edf14
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:36:18 -0500

    Create 9.1.9.py

---
+++ 9.final work on sets/9.1.Part1/9.1.9.py
@@ -0,0 +1,9 @@
+# Какие элементы будут храниться в множестве set3 в результате выполнения следующего программного кода? 
+# What elements will be stored in set3 when the following code is executed?
+
+set1 = {'Yellow', 'Orange', 'Black'}
+set2 = {'Orange', 'Blue', 'Pink'}
+set3 = set2.difference(set1)
+
+# Blue
+# Pink
