564c37ce7053cf2b3b74bec992d8d1f73b802035
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 21:31:31 -0500

    Create 8.5.5.py

---
+++ 8.Sets/8.5.Set methods/8.5.5.py
@@ -0,0 +1,5 @@
+# В чем разница между методами множеств discard() и remove()?
+# What is the difference between the discard() and remove() set methods?
+
+метод remove() вызывает исключение если элемент не найден в множестве
+метод discard() вызывает исключение если элемент не найден в множестве
