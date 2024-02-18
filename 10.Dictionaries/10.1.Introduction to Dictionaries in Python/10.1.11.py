a5d56e52a455e6a3e68bb9cff688f1d6a710b880
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 16:40:58 -0500

    Create 10.1.11.py

---
+++ 10.Dictionaries/10.1.Introduction to Dictionaries in Python/10.1.11.py
@@ -0,0 +1,7 @@
+# Что покажет результат выполнения приведенного ниже фрагмента кода?
+# What will be the result of running the following code snippet?
+
+my_dict = {1: [0, 1], 2: [2, 3], 3: [4, 5]}
+print(my_dict[2][1])
+
+# 3
