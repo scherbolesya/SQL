32f7da14bd61acbebe4152eb39754a3209bc9f90
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 21:30:29 -0500

    Create 8.5.4.py

---
+++ 8.Sets/8.5.Set methods/8.5.4.py
@@ -0,0 +1,7 @@
+# Select all methods to remove only the Orange element from the myset set:
+# Выберите все способы, позволяющие удалить только элемент Orange из множества myset:
+
+myset = {'Yellow', 'Orange', 'Black'}
+
+# myset.remove('Orange')
+# myset.discard('Orange')
