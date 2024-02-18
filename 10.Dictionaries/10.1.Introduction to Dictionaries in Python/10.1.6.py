2dc13a2c8b024358415c166c5ec60ff0015b5212
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 16:36:11 -0500

    Create 10.1.6.py

---
+++ 10.Dictionaries/10.1.Introduction to Dictionaries in Python/10.1.6.py
@@ -0,0 +1,9 @@
+# Каким образом можно создать словарь в котором ключом является страна, а значением ее столица?
+# How can you create a dictionary in which the key is the country, and the value is its capital?
+
+Country	Capital
+Canada	Ottawa
+Mexico	Mexico City
+United States	Washington, D.C.
+
+{'Canada': 'Ottawa', 'United States': 'Washington, D.C.', 'Mexico': 'Mexico City'}
