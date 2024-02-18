d2c1730be9341589a21804c8e3b3b0c21109868e
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 21:06:17 -0500

    Create 8.4.4.py

---
+++ 8.Sets/8.4.Basics sets/8.4.4.py
@@ -0,0 +1,8 @@
+# The programmer wrote a program consisting of one line
+# Программист написал программу, состоящую из одной строки
+print(len(set(input())))
+# Что выведет эта программа при вводе следующих строк:
+# What will this program output when you enter the following lines:
+
+# Beegeek - 4
+# карабас-барабас - 6
