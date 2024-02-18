b8329b3362a81b3ac98809ded74e065d7a13723e
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:33:08 -0500

    Create 9.1.6.py

---
+++ 9.final work on sets/9.1.Part1/9.1.6.py
@@ -0,0 +1,5 @@
+# Верно ли утверждение: метод symmetric_difference() возвращает множество, содержащее все элементы исходных множеств, не принадлежащие одновременно обоим исходным множествам.
+# Is the statement true: the symmetric_difference() method returns a set containing all elements of the original sets that do not belong to both original sets at the same time.
+
+# да
+# yes
