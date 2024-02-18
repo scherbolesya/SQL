8546e7e0ab1d7370b9cf7c7c791d446d81527587
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 16:24:16 -0500

    Update 9.2.11.BEEGEEK 4 Online School.py

--- 9.final work on sets/9.2.Part2/9.2.11.BEEGEEK 4 Online School.py
+++ 9.final work on sets/9.2.Part2/9.2.11.BEEGEEK 4 Online School.py
@@ -1,29 +1,52 @@
-# BEEGEEK 4 Online School
-# The head of the BEEGEEK online school and his assistant compiled lists of students from their school.
-# Write a program that will display all the names of the students that the leader and his assistant remember.
+# BEEGEEK 5 Online School 🌶️
+# Every student studying at the BEEGEEK online school studies either mathematics or computer science, or both of these subjects. 
+# The head of the school has lists of students studying each subject. By chance, the lists of all the students got mixed up.
+# Write a program that allows a teacher to find out how many students are studying only one subject.
 # Note. It is guaranteed that there are no namesakes among BEEGEEK school students.
 
-# Онлайн-школа BEEGEEK 4
-# Руководитель онлайн-школы BEEGEEK и его помощник составили списки учеников их школы.
-# Напишите программу, которая выведет все фамилии учеников, которые вспомнили руководитель и его помощник.
+# Онлайн-школа BEEGEEK 5 🌶️
+# Каждый ученик, обучающийся в онлайн-школе BEEGEEK изучает либо математику, либо информатику, либо оба этих предмета. 
+# У руководителя школы есть списки учеников, изучающих каждый предмет. Случайно списки всех учеников перемешались.
+# Напишите программу, которая позволит руководителю выяснить, сколько учеников изучает только один предмет.
 # Примечание. Гарантируется, что среди учеников школы BEEGEEK нет однофамильцев.
 
-set1 = {i for i in input().split()}
-set2 = {i for i in input().split()}
-print(*sorted(set1|set2))
+m,n = int(input()), int(input())
+mat = set()
+inf = set()
+for i in range(m+n):
+    s = input()
+    if len(mat) != m:
+        if s not in mat:
+            mat.add(s)
+        else:
+            inf.add(s)
+    else:
+        if s not in inf:
+            inf.add(s)
+a = mat.difference(inf) | inf.difference(mat) 
+if len(a) == 0:
+    print('NO')
+else:
+    print(len(a))
 
 Sample Input 1:
-Блинов Жданов
-Бобров Жданов Некрасов Блинов
+2
+3
+Хохлов
+Фадеев
+Ершов
+Ушаков
+Хохлов
 Sample Output 1:
-Блинов Бобров Жданов Некрасов
+3
 Sample Input 2:
+5
+1
+Игнатов
+Мухин
+Сафонов
+Калашников
+Демин
 Рыбаков
-Сафонов Игнатов
 Sample Output 2:
-Игнатов Рыбаков Сафонов
-Sample Input 3:
-Демин Рыбаков Сафонов Игнатов Мухин
-Сафонов Игнатов Демин Рыбаков
-Sample Output 3:
-Демин Игнатов Мухин Рыбаков Сафонов
+6
