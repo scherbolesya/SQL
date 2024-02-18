b6af39cc047f4e92f1e87d410ae3659dd124559f
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 21:59:41 -0500

    Create 8.6.4.py

---
+++ 8.Sets/8.6.Set Methods/8.6.4.py
@@ -0,0 +1,12 @@
+# Какие элементы будут храниться в множестве set3 в результате выполнения следующего программного кода? 
+# What elements will be stored in set3 when the following code is executed?
+
+set1 = set([100, 20, 30])
+set2 = set([100, 200, 300])
+set3 = set1.union(set2)
+
+# 200
+# 20
+# 30
+# 100
+# 300
