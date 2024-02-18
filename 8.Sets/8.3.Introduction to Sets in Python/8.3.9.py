14e5e40716dd524b3be22c0ef01e053491346403
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 20:49:27 -0500

    Create 8.3.9.py

---
+++ 8.Sets/8.3.Introduction to Sets in Python/8.3.9.py
@@ -0,0 +1,13 @@
+# What elements will be stored in myset after the following statement is executed?
+# Какие элементы будут храниться в множестве myset после исполнения приведенной ниже инструкции?
+
+# Note. The symbol ⎵ is used to indicate a space.
+# Примечание. Для обозначения пробела используется символ ⎵. 
+
+myset = set('ъъъ эээ ююю яяя')
+
+# я
+# э
+# ⎵
+# ъ
+# ю
