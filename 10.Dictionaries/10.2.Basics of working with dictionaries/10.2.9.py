863e6fafd42be7264f2d307a98e44a29b9f38651
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 18:46:32 -0500

    Create 10.2.9.py

---
+++ 10.Dictionaries/10.2.Basics of working with dictionaries/10.2.9.py
@@ -0,0 +1,9 @@
+# Что покажет приведенный ниже фрагмент кода? (порядок элементов может отличаться)
+# What will the code snippet below show? (item order may vary)
+
+my_dict = {1: [0, 1], 2: [2, 3], 3: [4, 5]}
+for k in my_dict.values():
+    print(k)
+# [0, 1]
+# [2, 3]
+# [4, 5]
