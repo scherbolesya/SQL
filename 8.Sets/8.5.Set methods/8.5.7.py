375adbf8e8ca9c0c3cd2a80cc9c6372f03309573
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 21:35:17 -0500

    Create 8.5.7.py

---
+++ 8.Sets/8.5.Set methods/8.5.7.py
@@ -0,0 +1,8 @@
+# Что будет выведено в результате выполнения следующего программного кода?
+# What will be the output of the following code?
+
+myset = {'python'}
+item = myset.pop()
+print(item, len(myset))
+
+# python 0
