9a289cec43702d97ab6757f6a90ec5e20cacb2eb
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:24:21 -0500

    Create 8.8.13.py

---
+++ 8.Sets/8.8.Set generators and frozenset/8.8.13.py
@@ -0,0 +1,12 @@
+# Выберите методы, отсутствующие у замороженных множеств (тип frozenset).
+# Select methods that frozen sets do not have (type frozenset).
+
+add()
+discard()
+symmetric_difference_update()
+intersection_update()
+difference_update()
+remove()
+update()
+pop()
+clear()
