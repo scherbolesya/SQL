964624f0b71c63cbff8ba120365a5baa7fdbd155
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:54:09 -0500

    Create 9.2.2.Homework.py

---
+++ 9.final work on sets/9.2.Part2/9.2.2.Homework.py
@@ -0,0 +1,28 @@
+# Homework
+# The teacher checks homework in class and received the following answers: out of n schoolchildren, 
+# m had their homework eaten by a dog, k had their lights turned off, and p students suffered both misfortunes. 
+# Write a program that determines how many people have completed their homework.
+# Input format
+# The numbers n,m,k,p are given as input to the program, each on a separate line.
+# Output format
+# The program should display the number of students who have completed their homework.
+
+# Домашнее задание
+# Учитель проверяет домашнее задание в классе и получил следующие ответы: 
+# из n школьников у m домашнее задание съела собака, у k отключили свет, 
+# а p учеников постигли оба несчастья. Напишите программу, которая определяет сколько человек выполнило домашнее задание.
+# Формат входных данных
+# На вход программе подаются числа n,m,k,p, каждое на отдельной строке.
+# Формат выходных данных
+# Программа должна вывести количество учеников, сделавших домашнее задание.
+
+n, m, k, p = [int(input()) for _ in range(4)]
+print(n + p - m - k)
+
+Sample Input:
+35
+20
+10
+3
+Sample Output:
+8
