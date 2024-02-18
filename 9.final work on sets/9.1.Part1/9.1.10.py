4e9a8e680bde00183f2e972b58d790e171abdf9c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:37:41 -0500

    Create 9.1.10.py

---
+++ 9.final work on sets/9.1.Part1/9.1.10.py
@@ -0,0 +1,9 @@
+# Какие элементы будут храниться в множестве set1 в результате выполнения следующего программного кода? 
+# What elements will be stored in set1 as a result of executing the following code?
+
+set1 = {'Yellow', 'Orange', 'Black'}
+set2 = {'Orange', 'Blue', 'Pink'}
+set1.difference_update(set2)
+
+# Yellow
+# Black
