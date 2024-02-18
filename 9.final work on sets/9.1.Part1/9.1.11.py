7e4298440e1501f016d203a9fdaf501fd6959b8a
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:39:49 -0500

    Create 9.1.11.py

---
+++ 9.final work on sets/9.1.Part1/9.1.11.py
@@ -0,0 +1,7 @@
+# Что может быть выведено в результате выполнения следующего программного кода? 
+
+myset = {'Yellow', 'Orange', 'Black'}
+myset.update(['Blue', 'Green', 'Red', 'Orange'])
+print(myset)
+
+# {'Yellow', 'Orange', 'Red', 'Black', 'Green', 'Blue'}
