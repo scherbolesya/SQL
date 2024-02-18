797e67bcf400798b72b4bb842cb5bc5139acf3ae
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 16:39:00 -0500

    Create 10.1.9.py

---
+++ 10.Dictionaries/10.1.Introduction to Dictionaries in Python/10.1.9.py
@@ -0,0 +1,7 @@
+# Что покажет результат выполнения приведенного ниже фрагмента кода?
+# What will be the result of running the following code snippet?
+
+my_dict = dict.fromkeys(['a', 'b', 'c'], -1)
+print(my_dict['b'])
+
+# -1
