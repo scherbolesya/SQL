654b1f8215a1c4b7725f75fc68c0613d4db67cc7
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 22:03:32 -0500

    Create 8.6.7.py

---
+++ 8.Sets/8.6.Set Methods/8.6.7.py
@@ -0,0 +1,9 @@
+# Какие элементы будут храниться в множестве set3 в результате выполнения следующего программного кода? 
+# What elements will be stored in set3 when the following code is executed?
+
+set1 = set([1, 2, 3, 4])
+set2 = set([3, 4, 5, 6])
+set3 = set2.difference(set1)
+
+# 6
+# 5
