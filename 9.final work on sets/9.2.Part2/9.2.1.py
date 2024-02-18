4e0aee8cce18d0cca36d2256551ad9e015385d90
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:50:35 -0500

    Create 9.2.1.py

---
+++ 9.final work on sets/9.2.Part2/9.2.1.py
@@ -0,0 +1,17 @@
+# В таблице приведены запросы и количество страниц, которые нашел поисковый сервер Google по этим запросам в сегменте Интернета:
+# The table shows the queries and the number of pages that the Google search engine found for these queries in the Internet segment:
+
+Запрос	                     Количество страниц (тыс.)
+математика & ЕГЭ	           315
+математика & параметры	      225
+математика & параметры & ЕГЭ	110
+
+# Какое количество страниц (в тысячах) будет найдено по запросу (параметры | ЕГЭ) & математика?
+# Примечание. В языке запросов поискового сервера для обозначения логической операции «ИЛИ» (объединение) используется символ «|», 
+# а для обозначения логической операции «И» (пересечение) — символ «&».
+
+# How many pages (in thousands) will be found for the query (parameters | Unified State Examination) & mathematics?
+# Note. In the search engine query language, the symbol "|" is used to denote the logical operation "OR" (union), 
+# and the symbol "&" is used to denote the logical operation "AND" (intersection).
+
+# 430
