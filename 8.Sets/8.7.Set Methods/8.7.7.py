519ba8164e49463465f56f17fc87d4665dad5b28
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 22:27:29 -0500

    Create 8.7.7.py

---
+++ 8.Sets/8.7.Set Methods/8.7.7.py
@@ -0,0 +1,8 @@
+# Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+
+set1 = set(range(1, 10))
+set2 = set(range(10, 20))
+print(set1.isdisjoint(set2))
+
+# True
