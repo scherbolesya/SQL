fa340267fff5eae2243aca6f6fdf1476755009cf
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 16:38:05 -0500

    Create 10.1.8.py

---
+++ 10.Dictionaries/10.1.Introduction to Dictionaries in Python/10.1.8.py
@@ -0,0 +1,7 @@
+# Что даст выполнение приведенного ниже фрагмента кода?
+# What will executing the code snippet below accomplish?
+
+my_dict = dict([('first', 1), ('second', 2), ('third', 3)])
+print(my_dict)
+
+# {'first': 1, 'second': 2, 'third': 3}
