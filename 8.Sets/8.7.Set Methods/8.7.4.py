517173b4d8a757cd79ba43df1d97a55782080238
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 22:24:11 -0500

    Create 8.7.4.py

---
+++ 8.Sets/8.7.Set Methods/8.7.4.py
@@ -0,0 +1,9 @@
+# Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+
+word = 'beegeek'
+set1 = set(word*3)
+set2 = set(word[::-1]*2 + 'stepik')
+print(set1 < set2)
+
+# True
