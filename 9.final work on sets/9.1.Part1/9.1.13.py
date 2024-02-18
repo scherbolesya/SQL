f37fe932370d5f5dcb99bad238f281dff4ff6d51
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 15:41:58 -0500

    Create 9.1.13.py

---
+++ 9.final work on sets/9.1.Part1/9.1.13.py
@@ -0,0 +1,10 @@
+# Что будет выведено в результате выполнения следующего программного кода?
+# What will be the output of the following code?
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {60, 70, 10, 30, 40, 80, 20, 50}
+print(set1.issubset(set2))
+print(set2.issuperset(set1))
+
+# True
+# True
