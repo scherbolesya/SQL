8727e474edd98e04672ce48cfd214c5891c61d77
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 21:07:11 -0500

    Create 8.4.5.py

---
+++ 8.Sets/8.4.Basics sets/8.4.5.py
@@ -0,0 +1,7 @@
+# Что выведет следующий программный код?
+# What will the following program code output?
+
+myset = {'Yellow', 'Orange', 'Black'}
+print(myset[1])
+
+# ошибка во время выполнения кода
