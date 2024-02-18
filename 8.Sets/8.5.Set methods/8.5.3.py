17082eb5c4f289bb834fe858112e40badbe7a693
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 21:29:25 -0500

    Create 8.5.3.py

---
+++ 8.Sets/8.5.Set methods/8.5.3.py
@@ -0,0 +1,9 @@
+# Что может быть выведено в результате выполнения следующего программного кода? 
+# What might be output as a result of executing the following code?
+
+myset = {'Yellow', 'Orange', 'Black'}
+myset.add('Blue')
+myset.add('Orange')
+print(myset)
+
+# {'Blue', 'Orange', 'Yellow', 'Black'}
