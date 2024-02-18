e9e881981d6dc734e8ffb5106d1180f06d0943f1
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-17 20:46:23 -0500

    Create 8.3.8.py

---
+++ 8.Sets/8.3.Introduction to Sets in Python/8.3.8.py
@@ -0,0 +1,10 @@
+# Какие элементы будут храниться в множестве myset после исполнения приведенной ниже инструкции?
+
+# What elements will be stored in myset after the following statement is executed?
+
+myset = set(range(2, 10, 2))
+
+# 4
+# 2
+# 8
+# 6
