979ac8da777a795102cd8f5f9e008e5fc02f909c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 18:47:40 -0500

    Create 10.2.10.py

---
+++ 10.Dictionaries/10.2.Basics of working with dictionaries/10.2.10.py
@@ -0,0 +1,4 @@
+# Какой словарный метод возвращает все ключи словаря и связанные с ними значения в виде последовательности кортежей?
+# Which dictionary method returns all the dictionary keys and their associated values as a sequence of tuples?
+
+items()
