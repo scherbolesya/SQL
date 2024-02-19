aea61e188ab45a4d10ee580c7134011f173eae9f
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 19:46:10 -0500

    Create 10.3.5.py

---
+++ 10.Dictionaries/10.3.Dictionary Methods/10.3.5.py
@@ -0,0 +1,4 @@
+# Какой метод возвращает значение, связанное с заданным ключом, и удаляет из словаря пару ключ / значение?
+# Which method returns the value associated with a given key and removes the key/value pair from the dictionary?
+
+#pop()
