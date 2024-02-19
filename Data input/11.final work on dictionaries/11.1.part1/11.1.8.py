6792680505e0f84af52faa132c70bc0027715bca
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-19 18:14:59 -0500

    Create 11.1.8.py

---
+++ Data input/11.final work on dictionaries/11.1.part1/11.1.8.py
@@ -0,0 +1,7 @@
+# Выберите все способы, с помощью которых можно получить значение по ключу marks:
+# Select all the ways you can get the value of the marks key:
+
+student = {'name': 'Emma', 'class': 9, 'marks': 75}
+
+# student['marks']
+# student.get('marks')
