f8cabb53fd377d934b20c07ac8607daa47795125
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 19:55:39 -0500

    Create 10.3.13.py

---
+++ 10.Dictionaries/10.3.Dictionary Methods/10.3.13.py
@@ -0,0 +1,9 @@
+# Дополните приведенный код, чтобы он вывел наиболее часто встречающееся слово строки s. Если таких слов несколько, должно быть выведено то, что меньше в лексикографическом порядке.
+# Complete the code below to print the most frequently occurring word in the string s. If there are several such words, the one that is smaller in lexicographical order should be output.
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+result = {}
+s1 = set(s.split())
+for i in s1:
+    key = s.count(i)
+    result[key] = result.get(key, []) + [i]
+print(sorted(result[max(result.keys())])[0])
