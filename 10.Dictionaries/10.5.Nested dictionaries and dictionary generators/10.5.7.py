23e5baae467ca82614076df45734deebac847920
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 21:49:00 -0500

    Create 10.5.7.py

---
+++ 10.Dictionaries/10.5.Nested dictionaries and dictionary generators/10.5.7.py
@@ -0,0 +1,8 @@
+# Дополните приведенный код, используя генератор, чтобы получить словарь result, состоящий из всех элементов словаря colors, кроме тех, у которых значением является None.
+# Примечание. Выводить содержимое словаря result не нужно.
+
+# Complete the following code using a generator to produce a result dictionary consisting of all the elements in the colors dictionary except those whose value is None.
+# Note. There is no need to display the contents of the result dictionary.
+
+colors = {'c1': 'Red', 'c2': 'Grey', 'c3': None, 'c4': 'Green', 'c5': 'Yellow', 'c6': 'Pink', 'c7': 'Orange', 'c8': None, 'c9': 'White', 'c10': 'Black', 'c11': 'Violet', 'c12': 'Gold', 'c13': None, 'c14': 'Amber', 'c15': 'Azure', 'c16': 'Beige', 'c17': 'Bronze', 'c18': None, 'c19': 'Lilac', 'c20': 'Pearl', 'c21': None, 'c22': 'Sand', 'c23': None}
+result = {k: colors[k] for k in colors if colors[k] != None}
