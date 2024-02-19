0a4de5407998eadba26977bcba1348f3455f9b89
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 19:45:21 -0500

    Create 10.3.4.py

---
+++ 10.Dictionaries/10.3.Dictionary Methods/10.3.4.py
@@ -0,0 +1,4 @@
+# Какой метод возвращает последний добавленный в словарь элемент (удаляя его из словаря) в виде кортежа (ключ, значение)?
+# Which method returns the last element added to the dictionary (removing it from the dictionary) as a (key, value) tuple?
+
+# popitem()
