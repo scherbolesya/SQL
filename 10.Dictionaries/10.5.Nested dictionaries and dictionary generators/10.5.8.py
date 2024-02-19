235d3b0165eb52c67f1bcaadaac5422dbd24dcdf
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 21:50:13 -0500

    Create 10.5.8.py

---
+++ 10.Dictionaries/10.5.Nested dictionaries and dictionary generators/10.5.8.py
@@ -0,0 +1,8 @@
+# Дополните приведенный код, используя генератор, чтобы получить словарь result, состоящий из всех элементов словаря favorite_numbers , значения которых являются двузначными числами.
+# Примечание. Выводить содержимое словаря result не нужно.
+
+# Complete the following code using a generator to produce a result dictionary consisting of all the items in the favorite_numbers dictionary whose values are two-digit numbers.
+# Note. There is no need to display the contents of the result dictionary.
+
+favorite_numbers = {'timur': 17, 'ruslan': 7, 'larisa': 19, 'roman': 123, 'rebecca': 293, 'ronald': 76, 'dorothy': 62, 'harold': 36, 'matt': 314, 'kim': 451, 'rosaly': 18, 'rustam': 89, 'soltan': 111, 'amir': 654, 'dima': 390, 'amiran': 777, 'geor': 999, 'sveta': 75, 'rita': 909, 'kirill': 404, 'olga': 271, 'anna': 55, 'madlen': 876}
+result = {key: value for key,value in favorite_numbers.items() if len(str(value)) == 2}
