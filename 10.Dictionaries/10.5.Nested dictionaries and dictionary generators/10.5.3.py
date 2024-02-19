95d114bbf266d7431b15a52a38110ba749a68b31
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 21:43:12 -0500

    Create 10.5.3.py

---
+++ 10.Dictionaries/10.5.Nested dictionaries and dictionary generators/10.5.3.py
@@ -0,0 +1,7 @@
+# Как извлечь из словаря students возраст Тимура?
+# How to extract Timur's age from the students dictionary?
+students = {1: {'name': 'Тимур', 'age': '28', 'sex': 'Male'},
+            2: {'name': 'Руслан', 'age': '22', 'sex': 'Male'},
+            3: {'name': 'Соня', 'age': '25', 'sex': 'Female'}}
+
+# students[1]['age']
