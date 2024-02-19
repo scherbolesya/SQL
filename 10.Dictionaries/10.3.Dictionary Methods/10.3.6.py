81c4f0bc9cc6fc0668335778ba752dfd21f8cccb
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-18 19:47:45 -0500

    Create 10.3.6.py

---
+++ 10.Dictionaries/10.3.Dictionary Methods/10.3.6.py
@@ -0,0 +1,4 @@
+# Какой метод возвращает значение, связанное с заданным ключом? Если ключ не найден, то он возвращает значение по умолчанию.
+# Which method returns the value associated with the given key? If the key is not found, then it returns the default value.
+
+# get()
