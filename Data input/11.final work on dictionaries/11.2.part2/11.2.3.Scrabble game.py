b95564bab1ae76f4d902db4e8245e78554061bc5
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-19 18:37:44 -0500

    Create 11.2.3.Scrabble game.py

---
+++ Data input/11.final work on dictionaries/11.2.part2/11.2.3.Scrabble game.py
@@ -0,0 +1,13 @@
+# Scrabble game
+# В игре Scrabble каждая буква приносит определенное количество баллов. Общая стоимость слова – сумма баллов его букв. Чем реже буква встречается, тем больше она ценится:
+
+# In the game Scrabble, each letter is worth a certain number of points. The total value of a word is the sum of the points of its letters. The less often a letter is found, the more valued it is:
+
+Баллы	Буква
+1	A, E, I, L, N, O, R, S, T, U
+2	D, G
+3	B, C, M, P
+4	F, H, V, W, Y
+5	K
+8	J, X
+10 Q, Z
