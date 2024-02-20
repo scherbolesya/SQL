07dd06f8d20402e4c4972475a0c6ad74dd48af7f
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 16:43:05 -0500

    Create 12.1.10.py

---
+++ 12.random and string modules/12.1.random module. Part 1/12.1.10.py
@@ -0,0 +1,6 @@
+# Функция seed() используется для инициализации начального значения генератора псевдослучайных чисел в модуле random. 
+# Верно ли, что по умолчанию, начальным значением генератора является системное время (текущая дата и время)?
+# The seed() function is used to initialize the seed value of the pseudo-random number generator in the random module. 
+# Is it true that by default, the initial value of the generator is the system time (current date and time)?
+
+да
