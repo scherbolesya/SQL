90c8564b498f34cf3003a8043b9560dcb4c0bfff
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 16:39:46 -0500

    Create 12.1.7.py

---
+++ 12.random and string modules/12.1.random module. Part 1/12.1.7.py
@@ -0,0 +1,10 @@
+# Какое число может быть в переменной num после выполнения следующего кода:
+# What number can be in the num variable after executing the following code:
+
+import random
+num = random.randrange(115)
+
+# 1
+# 50
+# 0
+# 114
