e7725a4697bb3b2e5ea50286a1d6b6c0cd203ef1
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 16:35:08 -0500

    Create 12.1.2.py

---
+++ 12.random and string modules/12.1.random module. Part 1/12.1.2.py
@@ -0,0 +1,3 @@
+# Как называется алгоритм, лежащий в основе генерации псевдослучайных чисел в модуле random?
+# What is the name of the algorithm underlying the generation of pseudo-random numbers in the random module?
+вихрь Мерсенна
