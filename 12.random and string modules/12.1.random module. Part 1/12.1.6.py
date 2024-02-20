827d057c541a75e30fa8fb0ab0a00c572f2c9e14
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 16:38:24 -0500

    Create 12.1.6.py

---
+++ 12.random and string modules/12.1.random module. Part 1/12.1.6.py
@@ -0,0 +1,3 @@
+# Какая функция возвращает случайное число с плавающей точкой внутри заданного диапазона значений?
+# Which function returns a random floating point number within a given range of values?
+uniform()
